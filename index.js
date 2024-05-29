35f231658d76f08e0ad0bfc267ea1668df17b99c
Hamza Samad <160117284+Hamzasamad55@users.noreply.github.com>
2024-05-29 15:33:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+#! usr/bin/env node
+"use strict";
+let myName = "Hamza Samad";
+console.log(myName);
+let myFatherName = "Muhammad Zafer-Uddin";
+console.log(myFatherName);
+let myBrothersName = "Azhar-Rizwan-faiz";
+console.log(myBrothersName);
---
+++ index.ts
@@ -0,0 +1,9 @@
+#! usr/bin/env node
+let myName: string = "Hamza Samad";
+console.log(myName);
+
+let myFatherName: string = "Muhammad Zafer-Uddin";
+console.log(myFatherName);
+
+let myBrothersName: string = "Azhar-Rizwan-faiz";
+console.log(myBrothersName);
